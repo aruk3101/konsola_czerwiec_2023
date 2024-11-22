ca98d3c93da9906e1403c862e33f75f4d23bf155
Damian Raczek <draczekprojekt@gmail.com>
2024-11-22 09:31:31 +0100

    Dodaj pliki projektów.

---
+++ konsola_czerwiec_2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace konsola_czerwiec_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

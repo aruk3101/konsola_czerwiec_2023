b0f0455e8d622b4f37c33d3deaf13bd35893f2f9
Damian Raczek <draczekprojekt@gmail.com>
2024-11-22 09:39:26 +0100

    Film class test in main function created.

--- konsola_czerwiec_2023/Program.cs
+++ konsola_czerwiec_2023/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            Film film1 = new Film();
+
+            Console.WriteLine($"Początkowy tytuł: {film1.GetTytul()}");
+            Console.WriteLine($"Początkowa liczba wypożyczeń: {film1.GetWypozyczenia()}");
+
+            film1.SetTytul("Zielona Mila");
+            Console.WriteLine($"Tytuł po ustawieniu: {film1.GetTytul()}");
+
+            Console.WriteLine($"Liczba wypożyczeń przed inkrementacją: {film1.GetWypozyczenia()}");
+            film1.Inkrementuj();
+            Console.WriteLine($"Liczba wypożyczeń po inkrementacji: {film1.GetWypozyczenia()}");
+
+            film1.Inkrementuj();
+            film1.Inkrementuj();
+            Console.WriteLine($"Liczba wypożyczeń po dwóch inkrementacjach: {film1.GetWypozyczenia()}");
         }
     }
 }
